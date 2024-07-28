c108cc4414c0e3c5ac376a3f2ed2c7a040a76707
ASpro <aszczepan5225@gmail.com>
2024-07-28 16:54:05 +0200

    Refactoring httpClient to smaller methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,41 +16,54 @@ public class Main {
     //free api website: https://jobicy.com/jobs-rss-feed
     private static final String MAIN_URI = "https://jobicy.com/api/v2/remote-jobs";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
+
     public static void main(String[] args) {
-        String requestParams = new ApiRequestParams()
-                .withGeo(ApiParamGeo.POLAND)
-                .withIndustry(ApiParamIndustry.DEV)
-                .toStringParams();
+        String uri = MAIN_URI + '?' + createApiRequestParams();
+        HttpRequest request = createGetRequestTo(uri);
+        String result = sendGet(request);
+        RemoteJobDetails jobDetails = mapJsonToObject(result);
+        System.out.println(jobDetails);
+    }
 
-        String uri = MAIN_URI + '?' + requestParams;
+    private static RemoteJobDetails mapJsonToObject(String result) {
+        try {
+            return OBJECT_MAPPER.readValue(result, RemoteJobDetails.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-        HttpRequest request = HttpRequest.newBuilder()
+    private static String sendGet(HttpRequest request) {
+        HttpClient httpClient = createHttpClient();
+        try {
+            return httpClient
+                    .send(request, HttpResponse.BodyHandlers.ofString())
+                    .body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static HttpClient createHttpClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+    }
+
+    private static HttpRequest createGetRequestTo(String uri) {
+        return HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(uri))
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json; utf-8")
                 .build();
+    }
 
-        HttpClient httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
-        
-        HttpResponse<String> response;
-        try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
-        String result = response.body();
-
-        RemoteJobDetails jobDetails;
-        try {
-            jobDetails = OBJECT_MAPPER.readValue(result, RemoteJobDetails.class);
-            System.out.println(jobDetails);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+    private static String createApiRequestParams() {
+        return new ApiRequestParams()
+                .withGeo(ApiParamGeo.POLAND)
+                .withIndustry(ApiParamIndustry.DEV)
+                .toStringParams();
     }
 }
