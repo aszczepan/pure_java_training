659ed7b62898daa7c9f5f4efe5e174f84007810f
ASpro <aszczepan5225@gmail.com>
2024-09-04 20:40:22 +0200

    Made simple http server with default '/' and '/names' uri

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,18 @@ package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.Executors;
 
 public class Main {
 
@@ -18,6 +23,45 @@ public class Main {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
 
     public static void main(String[] args) {
+//        receiveRemoteJobs();
+
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.setExecutor(Executors.newFixedThreadPool(10));
+
+            server.createContext("/", rootHandler());
+            server.createContext("/names", namesHandler());
+
+            server.start();
+            System.out.println("Server started on port 8080");
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static HttpHandler rootHandler() {
+        return exchange -> {
+            String response = "{\"message\":\"Hello, world!\"}";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            try (OutputStream outputStream = exchange.getResponseBody()) {
+                outputStream.write(response.getBytes());
+            }
+        };
+    }
+
+    private static HttpHandler namesHandler() {
+        return exchange -> {
+            String response = "{\"names\":[Ania, Wiktoria, Alicja, Weronika, Bartek, Jan, Katarzyna, Grzegorz]}";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            try (OutputStream outputStream = exchange.getResponseBody()) {
+                outputStream.write(response.getBytes());
+            }
+        };
+    }
+
+    private static void receiveRemoteJobs() {
         String uri = MAIN_URI + '?' + createApiRequestParams();
         HttpRequest request = createGetRequestTo(uri);
         String result = sendGet(request);
@@ -25,7 +69,7 @@ public class Main {
         System.out.println(jobDetails);
     }
 
-    private static RemoteJobDetails mapJsonToObject(String result) {
+    private static RemoteJobDetails mapJsonToObject(final String result) {
         try {
             return OBJECT_MAPPER.readValue(result, RemoteJobDetails.class);
         } catch (JsonProcessingException e) {
@@ -33,7 +77,7 @@ public class Main {
         }
     }
 
-    private static String sendGet(HttpRequest request) {
+    private static String sendGet(final HttpRequest request) {
         HttpClient httpClient = createHttpClient();
         try {
             return httpClient
@@ -51,7 +95,7 @@ public class Main {
                 .build();
     }
 
-    private static HttpRequest createGetRequestTo(String uri) {
+    private static HttpRequest createGetRequestTo(final String uri) {
         return HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(uri))
